dea23c23f52b60e2f581fa66a5b4dadf12fb2572
Samanta Graziela <samanta.graziela@gmail.com>
2024-08-22 09:39:07 -0300

    Adicionado teste lado 10

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,3 +9,8 @@ test('lado 2 para retornar area 24', () => {
 test('lado 3 para retornar volume 27', () => {
   expect(volumeCubo(3)).toBe(27);
 });
+
+// Testes para calculo da volume o cubo
+test('lado 10 para retornar volume 1000', () => {
+  expect(volumeCubo(10)).toBe(1000);
+});
